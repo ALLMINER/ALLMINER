8804eecb0d2342924928fe919b44390e404bf617
ALLMINER <MINERALNOV23@gmail.com>
2017-12-19 00:31:18 +0300

    optimal

--- 1)NVIDIA-algorithm-AUTO.bat
+++ 1)NVIDIA-algorithm-AUTO.bat
@@ -9,4 +9,4 @@ setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
 
-powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1   -Miningmode Automatic -PoolsName AHASHPOOL,MINING_POOL_HUB,ZPOOL -algorithm blake2s,phi,x17,ethash,neoscrypt,cryptonight,skunk,lyra2z,xevan,lyra2v2,nist5,veltor,blakecoin,hsr,c11,lbry,equihash,x11ghost,blake2b,blake14r,bitcore,groestl,timetravel
+powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1   -Miningmode Automatic -PoolsName AHASHPOOL,MINING_POOL_HUB,ZPOOL -algorithm blake2s,phi,x17,ethash,neoscrypt,cryptonight,skunk,lyra2z,xevan,lyra2v2,nist5,veltor,blakecoin,hsr,c11,lbry,equihash,x11ghost,blake2b,blake14r,bitcore,groestl,timetravel,skein
