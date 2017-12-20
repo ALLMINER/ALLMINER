8ac0a95ba66cb5cce809859d3d5591450f9835c9
ALLMINER <MINERALNOV23@gmail.com>
2017-12-20 20:29:52 +0300

    1

--- Start ALL AUTOS!!!!!!DANGER!!!!!!!!.bat
+++ Start ALL AUTOS!!!!!!DANGER!!!!!!!!.bat
@@ -9,5 +9,5 @@ setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
 
-powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1   -Miningmode Automatic -PoolsName AHASHPOOL,MINING_POOL_HUB,ZPOOL
+powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1   -Miningmode Automatic -pools ahashpool,zpool,mining_pool_hub,yiimp
 
